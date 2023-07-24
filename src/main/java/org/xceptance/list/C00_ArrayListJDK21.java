01aa7b4c3173f2e1d570702502df949d82f7061e
Rene Schwietzke <r.schwietzke@xceptance.net>
2023-07-24 23:08:42 +0200

    Testcase JDK 21 slowness list

--- src/main/java/org/xceptance/list/C00_ArrayListJDK21.java
+++ src/main/java/org/xceptance/list/C00_ArrayListJDK21.java
@@ -24,69 +24,25 @@ import org.openjdk.jmh.infra.BenchmarkParams;
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Warmup(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 10, time = 2, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
 
-public class C01_ArrayListGrow
+public class C00_ArrayListJDK21
 {
-    int iterationCount;
     String src;
-
     List<String> result;
-
     final String LONG = "R,CandleDaySalesPage.2,1666954266805,95,false,1349,429,200,https://production-test.justacmecompany.com/on/dishwasher.store/Sites-justacmecompany-Site/en_US/__Analytics-Start?url=https%3A%2F%2Fproduction-test.justacmecompany.com%2Fs%2Fjustacmecompany%2Fc%2Fhome-smellstuff%2Fworkhelp4life&res=1600x1200&cookie=1&cmpn=&java=0&gears=0&fla=0&ag=0&dir=0&pct=0&pdf=0&qt=0&realp=0&tz=US%2FEastern&wma=1&pcat=new-arrivals&title=3-Wick+Scented+Candles+-+Swim+%26+Swamp+Tier&dwac=0.7629667259452815&r=2905563956785988054&ref=https%3A%2F%2Fproduction-test.justacmecompany.com%2F&data=givemesomedatathatjustfillshere,image/gif,0,0,95,0,95,95,,GET,,,0,,";
 
     @Setup(Level.Iteration)
     public void setup(BenchmarkParams params) throws InterruptedException
     {
         src = new String(LONG.toCharArray());
-
-        iterationCount++;
-
-        int size = 0;
-
-        if (params.getBenchmark().endsWith("large_small"))
-        {
-            size = iterationCount <= params.getWarmup().getCount() ? 50 : 1;
-        }
-        else if (params.getBenchmark().endsWith("small_large"))
-        {
-            size = iterationCount <= params.getWarmup().getCount() ? 1 : 50;
-        }
-        else if (params.getBenchmark().endsWith("small_small"))
-        {
-            size = 1;
-        }
-        else if (params.getBenchmark().endsWith("large_large"))
-        {
-            size = 50;
-        }
-        System.out.printf("(Initial List Size %d) - ", size);
-        result= new ArrayList<>(size);
+        result= new ArrayList<>(50);
     }
 
     @Benchmark
-    public int m00_basicReadPerformance()
-    {
-        final int size = src.length();
-        int count = 0;
-
-        for (int pos = 0; pos < size; pos++)
-        {
-            char c = src.charAt(pos);
-
-            if (c == ',')
-            {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    @Benchmark
-    public List<String> m01_small_small()
+    public List<String> m01_plain()
     {
         result.clear();
         final int size = src.length();
@@ -105,9 +61,9 @@ public class C01_ArrayListGrow
     }
 
     @Benchmark
-    public List<String> m02_large_large()
+    public List<String> m02_noReuse()
     {
-        result.clear();
+        result = new ArrayList<>(50);
         final int size = src.length();
 
         for (int pos = 0; pos < size; pos++)
@@ -124,9 +80,9 @@ public class C01_ArrayListGrow
     }
 
     @Benchmark
-    public List<String> m04_large_small()
+    public List<String> m03_local()
     {
-        result.clear();
+        final List<String> list = new ArrayList<>(50);
         final int size = src.length();
 
         for (int pos = 0; pos < size; pos++)
@@ -135,29 +91,10 @@ public class C01_ArrayListGrow
 
             if (c == ',')
             {
-                result.add(src);
+                list.add(src);
             }
         }
 
-        return result;
-    }
-
-    @Benchmark
-    public List<String> m03_small_large()
-    {
-        result.clear();
-        final int size = src.length();
-
-        for (int pos = 0; pos < size; pos++)
-        {
-            char c = src.charAt(pos);
-
-            if (c == ',')
-            {
-                result.add(src);
-            }
-        }
-
-        return result;
+        return list;
     }
 }
