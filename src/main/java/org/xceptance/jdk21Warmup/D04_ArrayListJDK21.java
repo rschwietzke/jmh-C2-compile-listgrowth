d990b13f6d1ebd90e881d25cae31cd5b0fe51cec
Rene Schwietzke <r.schwietzke@xceptance.net>
2023-07-24 23:58:19 +0200

    Backup test cases

---
+++ src/main/java/org/xceptance/jdk21Warmup/D04_ArrayListJDK21.java
@@ -0,0 +1,66 @@
+package org.xceptance.jdk21Warmup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.BenchmarkParams;
+
+/**
+ * ArrayList Performance over time
+ * @author Rene Schwietzke <r.schwietzke@xceptance.com>
+ */
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 2, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+
+public class D04_ArrayListJDK21
+{
+    List<String> result;
+
+    String src = "R,CandleDaySalesPage.2,1666954266805,95,false,1349,429,200,https://production-test.justacmecompany.com/on/dishwasher.store/Sites-justacmecompany-Site/en_US/__Analytics-Start?url=https%3A%2F%2Fproduction-test.justacmecompany.com%2Fs%2Fjustacmecompany%2Fc%2Fhome-smellstuff%2Fworkhelp4life&res=1600x1200&cookie=1&cmpn=&java=0&gears=0&fla=0&ag=0&dir=0&pct=0&pdf=0&qt=0&realp=0&tz=US%2FEastern&wma=1&pcat=new-arrivals&title=3-Wick+Scented+Candles+-+Swim+%26+Swamp+Tier&dwac=0.7629667259452815&r=2905563956785988054&ref=https%3A%2F%2Fproduction-test.justacmecompany.com%2F&data=givemesomedatathatjustfillshere,image/gif,0,0,95,0,95,95,,GET,,,0,,";
+
+    @Setup
+    public void setup(BenchmarkParams params) throws InterruptedException
+    {
+        // src has to be "live" and not from the constant pool
+        src = new String(src.toCharArray());
+        result= new ArrayList<>(50);
+
+        // if we remove that, the warmup behavior of m00 is normal
+        String.format("(Initial List Size %d) - ", 50);
+    }
+
+    @Benchmark
+    public List<String> m00()
+    {
+        result.clear();
+        final int size = src.length();
+
+        for (int pos = 0; pos < size; pos++)
+        {
+            char c = src.charAt(pos);
+
+            if (c == ',')
+            {
+                result.add(src);
+            }
+        }
+
+        return result;
+    }
+}
